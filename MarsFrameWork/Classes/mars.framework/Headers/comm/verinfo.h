2ef14dcb775ad609cf3281a81b89187ca3d828b5
liuliangju <liuliangju@gome.com.cn>
2019-12-17 14:55:33 +0800

    git tag

---
+++ MarsFrameWork/Classes/mars.framework/Headers/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "810e303c"
+#define MARS_PATH "master"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2019-12-17 14:48:46"
+#define MARS_TAG ""
+
+#endif
